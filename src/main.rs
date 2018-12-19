7c3dd2d863c9505991b2e0d94f56c03522a141d0
zipkr <zipkr@gmail.com>
2018-12-18 22:43:13 -0800

    working shit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate serde_derive;
+
+extern crate rocket_contrib;
+extern crate serde; 
+
+use std::io::Read;
+use rocket_contrib::json::Json;
+use rocket::{Request, Data, Outcome, Outcome::*};
+use rocket::data::{self, FromDataSimple};
+use rocket::http::{Status, ContentType};
+
+const HOSTS: &'static [&'static str] = &["http://localhost:8000", "http://localhost:8001"];
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Message {
+    text: String,
+    host: String
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[post("/message", format="application/json", data = "<message>")]
+fn receive_message<'a>(message: Json<Message>) -> &'static str {
+    println!("{}", message.text);
+    println!("{}", message.host);
+    "Message Received"
+} 
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, receive_message]).launch();
+}
